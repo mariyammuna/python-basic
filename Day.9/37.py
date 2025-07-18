a287a1abe34287af92661e67e0021584dbcc6c91
mariyammuna <mariyam.std@gmail.com>
2025-07-18 21:38:20 +0600

    learning List Comprehension and condition

--- Day.9/37.py
+++ Day.9/37.py
@@ -3,9 +3,33 @@
 # [expressionb(element) for element in list condition]
 
 # Example 4: Using if with list comprehension
-
+# a = []
+# for i in range(1, 20):
+#     if i % 3 == 0:
+#         a.append(i)
+# print(a)
+# b = [i for i in range(1,20) if i % 3 == 0]
+# print(b)
 # Example 5: Nested if with list comprehension
-  
+# a = []
+# for i in range(1, 20):
+#     if i % 3 == 0:
+#          a.append(i)
+#     if i % 5 == 0:
+#          a.append(i)
+# print(a)
+# b = [i for i in range(1,20) if i % 3 == 0 or i % 5 == 0]
+# print(b)
 # Example 6: If...else with list comprehension
 # [if else for i in list]
 
+# a = []
+# for i in range(20):
+#     if i % 2 == 0:
+#         a.append("Even")
+#     else:
+#         a.append("Odd")
+# print(a)
+
+b = ["even" if i % 2 == 0 else "odd" for i in range(20)]
+print(b)
---
+++ Day.9/38.py
@@ -0,0 +1,14 @@
+# 2D List Comprehension
+matrix = [[1,2], [3,4], [5,6], [7,8]]
+# # Transpose matrix e conversion
+
+# a = []
+# for row in range(2):    # outer loop
+#     b = []
+#     for col in matrix:  # inner loop
+#         b.append(col[row])
+#         a.append(b)
+# print(a)
+
+# res = [[col[row] for col in matrix] for row in range(2)]
+# print(res)
